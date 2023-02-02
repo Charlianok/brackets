34e45d0b275529a1f03093bdad10fa217360ad17
Charlianok <66828674+Charlianok@users.noreply.github.com>
2023-02-02 17:17:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsString = bracketsConfig.join("").replace(/,/g, "");
+  let bracketsArray = [];
+  for (let bracket of str) {
+    let bracketsIndex = bracketsString.indexOf(bracket);
+    if (bracketsIndex % 2 === 0) {
+      if (bracket === bracketsString[bracketsIndex + 1] && bracketsArray[bracketsArray.length - 1] === bracketsIndex) {
+        bracketsArray.pop();
+      } else if (bracket === bracketsString[bracketsIndex + 1] && bracketsArray[bracketsArray.length - 1] !== bracketsIndex) {
+        bracketsArray.push(bracketsIndex);
+      } else {
+        bracketsArray.push(bracketsIndex);
+      }
+    } else {
+      if (bracketsArray.pop() !== bracketsIndex - 1) {
+        return false;
+      }
+    }
+  }
+  return bracketsArray.length === 0;
 }
